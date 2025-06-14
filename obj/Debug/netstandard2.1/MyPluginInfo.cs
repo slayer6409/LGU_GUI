278a6b288041342632467f353404869ccdc51e6b
slayer6409 <andrews.webs6409@gmail.com>
2025-06-14 03:14:22 -0700

    Delete obj/Debug/netstandard2.1 directory
    removed

--- obj/Debug/netstandard2.1/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace LGUGui
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "LGUGui";
-        public const string PLUGIN_NAME = "LGUGui";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
